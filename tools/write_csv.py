afa4efd14d07c04589e0e63ee06fbc60ebffaabb
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-06-09 19:20:07 -0500

    Update write_csv.py

--- tools/write_csv.py
+++ tools/write_csv.py
@@ -2,15 +2,15 @@ import csv
 from supervision.detection.core import Detections
 from icecream import ic
 
-def output_data_list(output: list, frame_number: int, data: Detections, class_names: dict) -> list:
+def output_data_list(output: list, frame_number: int, detections: Detections) -> list:
     """ Append object detection results to list """
-    for xyxy, _, confidence, class_id, tracker_id, _ in data:
-        x = int(xyxy[0])
-        y = int(xyxy[1])
-        w = int(xyxy[2]-xyxy[0])
-        h = int(xyxy[3]-xyxy[1])
+    for xyxy, _, confidence, _, tracker_id, data in detections:
+        x = xyxy[0]
+        y = xyxy[1]
+        w = xyxy[2]-xyxy[0]
+        h = xyxy[3]-xyxy[1]
         if tracker_id is not None:
-            output.append([frame_number, tracker_id, class_names[class_id], x, y, w, h, confidence])
+            output.append([frame_number, tracker_id, data['class_name'], x, y, w, h, confidence])
 
     return output
 
